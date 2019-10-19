bcc8ad734ce416a0338b56d11da15094ee997d11
Micah Ferrill <mcferrill@gmail.com>
2019-10-19 14:24:20 -0500

    Added local data for development.

--- functions.js
+++ functions.js
@@ -1,4 +1,14 @@
+
 function load(url, callback){
+
+    // Replace url with path to sample data if we're not in production
+    if (!window.location.href.includes('kos-widgets.netlify.com')){
+        if (url.includes('matches'))
+            url = '/data/matches.json';
+        else if (url.includes('tickers'))
+            url = '/data/tickers.json';
+    }
+
     var req = new XMLHttpRequest();
     req.overrideMimeType('application/json');
     req.open('GET', url, true);
